4569ad885e27a4a9beaee015801854cc45599179
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-11-21 19:38:23 +0300

    Commits

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,19 +1,10 @@
-#!/usr/bin/python3
-""" Init blueprint module """
 from flask import Blueprint
-from models.city import City
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.review import Review
-from models.amenity import Amenity
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.users import *
-from api.v1.views.index import *
-from api.v1.views.places import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.places_reviews import *
+from api.v1.views.index import *  # noqa
+from api.v1.views.states import *  # noqa
+from api.v1.views.cities import *  # noqa
+from api.v1.views.amenities import *  # noqa
+from api.v1.views.users import *  # noqa
+from api.v1.views.places import *  # noqa
+from api.v1.views.places_reviews import *  # noqa
+from api.v1.views.places_amenities import *  # noqa
